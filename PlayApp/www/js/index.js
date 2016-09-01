1b3d2b0d8e17eac1b82214b898173d9b4f4bf1ad
thegodofhellfire <stevenwatterson@yahoo.co.uk>
2016-09-01 21:35:53 +0100

    Update index.js

--- PlayApp/www/js/index.js
+++ PlayApp/www/js/index.js
@@ -17,9 +17,25 @@
  * under the License.
  */
 var app = {
+    // global vars
+    autoShowInterstitial: false,
+    progressDialog: document.getElementById("progressDialog"),
+    spinner: document.getElementById("spinner"),
+    weinre: {
+        enabled: false,
+        ip: '', // ex. http://192.168.1.13
+        port: '', // ex. 9090
+        targetApp: '' // ex. see weinre docs
+    },
+
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        if ((/(ipad|iphone|ipod|android)/i.test(navigator.userAgent))) {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        } else {
+            app.onDeviceReady();
+        }
     },
     // Bind Event Listeners
     //
@@ -34,6 +50,29 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        var weinre,
+            weinreUrl;
+
+        document.removeEventListener('deviceready', app.onDeviceReady, false);
+
+        if (app.weinre.enabled) {
+            console.log('Loading weinre...');
+            weinre = document.createElement('script');
+            weinreUrl = app.weinre.ip + ":" + app.weinre.port;
+            weinreUrl += '/target/target-script-min.js';
+            weinreUrl += '#' + app.weinre.targetApp;
+            weinre.setAttribute('src', weinreUrl);
+            document.head.appendChild(weinre);
+        }
+
+        if (window.admob) {
+            console.log('Binding ad events...');
+            app.bindAdEvents();
+            console.log('Initializing ads...');
+            app.initAds();
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,5 +84,155 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    }
+    
+        initAds: function () {
+        var isAndroid = (/(android)/i.test(navigator.userAgent));
+        var adPublisherIds = {
+            ios: {
+                banner: 'ca-app-pub-9863325511078756/5232547029',
+                interstitial: 'ca-app-pub-9863325511078756/6709280228'
+            },
+            android: {
+                banner: 'ca-app-pub-9863325511078756/9802347428',
+                interstitial: 'ca-app-pub-9863325511078756/2279080628'
+            }
+        };
+        var admobid;
+
+        if (isAndroid) {
+            admobid = adPublisherIds.android;
+        } else {
+            admobid = adPublisherIds.ios;
+        }
+        if (window.admob) {
+            admob.setOptions({
+                publisherId: admobid.banner,
+                interstitialAdId: admobid.interstitial,
+                bannerAtTop: true, // set to true, to put banner at top
+                overlap: false, // set to true, to allow banner overlap webview
+                offsetStatusBar: true, // set to true to avoid ios7 status bar overlap
+                isTesting: true, // receiving test ads (do not test with real ads as your account will be banned)
+                autoShowBanner: true, // auto show banners ad when loaded
+                autoShowInterstitial: false // auto show interstitials ad when loaded
+            });
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+
+    bindAdEvents: function () {
+        if (window.admob) {
+            document.addEventListener("orientationchange", this.onOrientationChange, false);
+            document.addEventListener(admob.events.onAdLoaded, this.onAdLoaded, false);
+            document.addEventListener(admob.events.onAdFailedToLoad, this.onAdFailedToLoad, false);
+            document.addEventListener(admob.events.onAdOpened, function (e) { }, false);
+            document.addEventListener(admob.events.onAdClosed, function (e) { }, false);
+            document.addEventListener(admob.events.onAdLeftApplication, function (e) { }, false);
+            document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) { }, false);
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+
+    onOrientationChange: function () {
+        app.onResize();
+    },
+    onAdLoaded: function (e) {
+        app.showProgress(false);
+        if (window.admob && e.adType === window.admob.AD_TYPE.INTERSTITIAL) {
+            if (app.autoShowInterstitial) {
+                window.admob.showInterstitialAd();
+            } else {
+                alert("Interstitial is available. Click on 'Show Interstitial' to show it.");
+            }
+        }
+    },
+    onAdFailedToLoad: function (e) {
+        app.showProgress(false);
+        alert("Could not load ad: " + JSON.stringify(e));
+    },
+    onResize: function () {
+        var msg = 'Web view size: ' + window.innerWidth + ' x ' + window.innerHeight;
+        document.getElementById('sizeinfo').innerHTML = msg;
+    },
+
+        startBannerAds: function () {
+        if (window.admob) {
+            app.showProgress(true);
+            window.admob.createBannerView(function () { }, function (e) {
+                alert(JSON.stringify(e));
+            });
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    removeBannerAds: function () {
+        if (window.admob) {
+            app.showProgress(false);
+            window.admob.destroyBannerView();
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    showBannerAds: function () {
+        if (window.admob) {
+            app.showProgress(false);
+            window.admob.showBannerAd(true, function () { }, function (e) {
+                alert(JSON.stringify(e));
+            });
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    hideBannerAds: function () {
+        if (window.admob) {
+            app.showProgress(false);
+            window.admob.showBannerAd(false);
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    requestInterstitial: function (autoshow) {
+        if (window.admob) {
+            app.showProgress(true);
+            app.autoShowInterstitial = autoshow;
+            window.admob.requestInterstitialAd(function () { }, function (e) {
+                alert(JSON.stringify(e));
+            });
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    showInterstitial: function () {
+        if (window.admob) {
+            app.showProgress(false);
+            window.admob.showInterstitialAd(function () { }, function (e) {
+                alert(JSON.stringify(e));
+            });
+        } else {
+            alert('cordova-admob plugin not ready.\nAre you in a desktop browser? It won\'t work...');
+        }
+    },
+    showProgress: function (show) {
+        if (show) {
+            addClass(app.spinner, "animated");
+            removeClass(app.progressDialog, "hidden");
+        } else {
+            addClass(app.progressDialog, "hidden");
+            removeClass(app.spinner, "animated");
+        }
+    },
+
+    function removeClass(elem, cls) {
+        var str;
+        do {
+            str = " " + elem.className + " ";
+            elem.className = str.replace(" " + cls + " ", " ").replace(/^\s+|\s+$/g, "");
+        } while (str.match(cls));
+    },
+
+    function addClass(elem, cls) {
+        elem.className += (" " + cls);
     }
 };
